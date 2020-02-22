88f10ccd6810dadbc64ddcab8f95b520e9fb4669
Soumendu Prasad Sinha <soumsps@gmail.com>
2020-02-22 23:02:22 +0530

    initial commit , implemented basic structure of web app

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+console.log("Written by Soumendu");
+
+// Algorithms IDs
+
+const algoData = {
+  id_1: "Linear Search",
+  id_2: "Binary Search",
+  id_3: "Selection Sort",
+  id_4: "Bubble Sort"
+};
+
+const algoChange = () => {
+  console.log("algo changed");
+};
+
+const dataSetSizeChange = () => {
+  console.log("data set changed");
+};
